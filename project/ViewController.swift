d56e3327ca983e3178a9997908aa9fa1caf72109
ctrlaltbelieve2 <ctrlaltbelieve2@ctrlaltbelieve2s-Mac-mini.local>
2015-01-23 01:32:31 -0800

    ui changed

---
+++ project/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  project
+//
+//  Created by ctrlaltbelieve2 on 2015-01-21.
+//  Copyright (c) 2015 ctrlaltbelieve2. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
