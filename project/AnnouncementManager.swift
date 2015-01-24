17105269102d6424d18be0adc45eb414fd0052c3
Justin Chee <JustinChee@Dove.local>
2015-01-24 01:41:10 -0800

    Front Page Modification
    Modified the front page to have the appropriate “control”.
    Cleaned up code in places.
    Removed dependencies by creating manager classes.

---
+++ project/AnnouncementManager.swift
@@ -0,0 +1,16 @@
+//
+//  AnnouncementManager.swift
+//  project
+//
+//  Created by Justin Chee on 2015-01-24.
+//  Copyright (c) 2015 ctrlaltbelieve2. All rights reserved.
+//
+
+import UIKit
+
+var announcementManager = AnnouncementManager();
+
+class AnnouncementManager: NSObject {
+    
+    var announcements = ["Classes Cancelled", "Quiz next week!", "New office hours", "test", "test", "test", "test", "teset", "test","testlast"];
+}
---
+++ project/GroupManager.swift
@@ -0,0 +1,16 @@
+//
+//  GroupManager.swift
+//  project
+//
+//  Created by Justin Chee on 2015-01-24.
+//  Copyright (c) 2015 ctrlaltbelieve2. All rights reserved.
+//
+
+import UIKit
+
+var groupManager = GroupManager();
+
+class GroupManager: NSObject {
+    
+    var groups = ["BLAW3600 Computers and the Law 4R", "BLAW3600 Computers and the Law 4X", "BLAW3600 Computers and the Law 4Y", "BLAW3600 Computers and the Law 4Z", "BLAW1100 The Law and Society 4A", "BLAW1100 The Law and Society 4B", "BLAW3600 The Law and Society 4C", "BLAW3600 The Law and Society 4D", "BLAW2235 Fun with the Law 2G", "BLAW1200 Suit up by Lawyering up 1F", "BLAW1200 Suit up by Lawyering up 1S"];
+}
