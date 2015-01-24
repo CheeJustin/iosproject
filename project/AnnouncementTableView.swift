29d605c343cb3c7a26ccff184bd972c6ea8c7b5b
Justin Chee <JustinChee@Dove.local>
2015-01-23 21:32:58 -0800

    Fixed Announcement Table
    Added compatibility so that it works with the view controller for a
    better design.

--- project/AnnouncementTableView.swift
+++ project/AnnouncementTableView.swift
@@ -8,44 +8,52 @@
 
 import UIKit
 
-class AnnouncementTableView: UITableViewController
+class AnnouncementTableView: UIViewController, UITableViewDelegate, UITableViewDataSource //UITableViewController
 {
 
-    var myAnnouncements: Array<AnyObject> = [];
+    var myAnnouncements: Array<AnyObject> = ["Classes Cancelled", "Quiz next week!", "New office hours"];
+    let cellID: String = "AnnouncementCell";
+    
+    @IBOutlet weak var tblAnnouncements: UITableView!
+    
     
     override func viewDidLoad()
     {
         super.viewDidLoad();
         
-        myAnnouncements = ["Classes Cancelled", "Quiz next week!", "New office hours"];
+        self.tblAnnouncements.registerClass(UITableViewCell.self, forCellReuseIdentifier: cellID)
+        self.tblAnnouncements.dataSource = self;
     }
     
-    override func numberOfSectionsInTableView(tableView: UITableView) -> Int
+    /*
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int
     {
         return 1;
     }
+    */
     
-    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int
     {
         return myAnnouncements.count;
     }
     
-    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell
+    {
         
-        let cellID: String = "AnnouncementCell";
-        let cell = tableView.dequeueReusableCellWithIdentifier(cellID) as UITableViewCell;
+        let cell = self.tblAnnouncements.dequeueReusableCellWithIdentifier(cellID) as UITableViewCell;
         
         cell.textLabel?.text = (myAnnouncements[indexPath.row] as String);
         
         return cell;
     }
+    /*
     
-    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool
+    func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool
     {
         return true;
     }
     
-    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath)
+    func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath)
     {
         if editingStyle == .Delete
         {
@@ -57,5 +65,6 @@ class AnnouncementTableView: UITableViewController
             // Stuff
         }
     }
+    */
     
 }
